c3e358a8d50d9fc87dfa803309c1694cf7b05138
sathyat20 <sathya.thenappan.23@dartmouth.edu>
2023-11-14 21:31:22 +0800

    Fixed route/contoller for retrieving applications for user based on status

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,8 @@ const BaseController = require("./baseController");
 class UserController extends BaseController {
   constructor(userModel, applicationsModel, applicationStatusModel) {
     super(userModel);
-    this.applicationsModel = applicationsModel
-    this.applicationStatusModel = applicationStatusModel
+    this.applicationsModel = applicationsModel;
+    this.applicationStatusModel = applicationStatusModel;
   }
 
   test = (req, res) => {
@@ -15,47 +15,92 @@ class UserController extends BaseController {
   createOne = async (req, res) => {
     const { email, firstName, lastName, username, password, profilePic } =
       req.body;
-      //input validation
+    //input validation
 
-      if (!email || !firstName || !lastName || !username || !password || !profilePic) {
-        return res.status(400).json({sucess: false, msg: "Please ensure all inputs are in"
-        })
-      }
-      try{
-        console.log("body:", req.body)
-        const newUser = await this.model.create({
-      email,
-      firstName,
-      lastName,
-      username,
-      password,
-      profilePic,
-    });
-    
-    return res.json({ success: true, user: newUser });}
-    catch (err) {
-      return res.status(400).json({success: false, msg: err})
+    if (
+      !email ||
+      !firstName ||
+      !lastName ||
+      !username ||
+      !password ||
+      !profilePic
+    ) {
+      return res
+        .status(400)
+        .json({ sucess: false, msg: "Please ensure all inputs are in" });
+    }
+    try {
+      console.log("body:", req.body);
+      const newUser = await this.model.create({
+        email,
+        firstName,
+        lastName,
+        username,
+        password,
+        profilePic,
+      });
+
+      return res.json({ success: true, user: newUser });
+    } catch (err) {
+      return res.status(400).json({ success: false, msg: err });
     }
   };
 
   getUserApplications = async (req, res) => {
-    const {userId} = req.params;
+    const { userId } = req.params;
 
     try {
       const userWithApplications = await this.model.findOne({
-        where: {id: userId},
-        include: [{ model: this.applicationsModel, include: this.applicationStatusModel }]
+        where: { id: userId },
+        include: [
+          {
+            model: this.applicationsModel,
+            include: this.applicationStatusModel,
+          },
+        ],
       });
 
       if (!userWithApplications) {
-        return res.status(404).json({ success: false, msg: "User not found"});
+        return res.status(404).json({ success: false, msg: "User not found" });
       }
 
-      return res.json({ success: true, applications: userWithApplications.applications })
+      return res.json({
+        success: true,
+        applications: userWithApplications.applications,
+      });
     } catch (err) {
-      return res.status(500).json({ success: false, msg: err.message })
+      return res.status(500).json({ success: false, msg: err.message });
     }
-  }
+  };
+
+  // To retrieve job applications for each user based on status_id
+
+  getApplicationsByStatus = async (req, res) => {
+    const { userId, statusId } = req.params;
+
+    try {
+      const userWithFilteredApplications = await this.model.findOne({
+        where: { id : userId },
+        include: [
+          {
+            model: this.applicationsModel,
+            where: {statusId: statusId},
+            include: [this.applicationStatusModel]
+          }
+        ],
+      });
+
+      if (!userWithFilteredApplications || !userWithFilteredApplications.applications.length) {
+        return res
+          .status(404)
+          .json({ success: false, msg: "No applications found for that status for the user or user doesnt exist" });
+      }
+
+      return res.json({ success: true, applications: userWithFilteredApplications.applications });
+    } catch (err) {
+      return res.status(500).json({ success: false, msg: err.message });
+    }
+  };
 }
 
 module.exports = UserController;
