7ec07f03ef759a6270f334258257f017cffa3fd3
gbrllim <gbrllim@gmail.com>
2023-11-14 21:29:01 +0800

    Added method to retrieve a single applicationId from a single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,8 @@ const BaseController = require("./baseController");
 class UserController extends BaseController {
   constructor(userModel, applicationsModel, applicationStatusModel) {
     super(userModel);
-    this.applicationsModel = applicationsModel
-    this.applicationStatusModel = applicationStatusModel
+    this.applicationsModel = applicationsModel;
+    this.applicationStatusModel = applicationStatusModel;
   }
 
   test = (req, res) => {
@@ -15,47 +15,104 @@ class UserController extends BaseController {
   createOne = async (req, res) => {
     const { email, firstName, lastName, username, password, profilePic } =
       req.body;
-      //input validation
+    //input validation
 
-      if (!email || !firstName || !lastName || !username || !password || !profilePic) {
-        return res.status(400).json({sucess: false, msg: "Please ensure all inputs are in"
-        })
-      }
-      try{
-        console.log("body:", req.body)
-        const newUser = await this.model.create({
-      email,
-      firstName,
-      lastName,
-      username,
-      password,
-      profilePic,
-    });
-    
-    return res.json({ success: true, user: newUser });}
-    catch (err) {
-      return res.status(400).json({success: false, msg: err})
+    if (
+      !email ||
+      !firstName ||
+      !lastName ||
+      !username ||
+      !password ||
+      !profilePic
+    ) {
+      return res
+        .status(400)
+        .json({ sucess: false, msg: "Please ensure all inputs are in" });
+    }
+    try {
+      console.log("body:", req.body);
+      const newUser = await this.model.create({
+        email,
+        firstName,
+        lastName,
+        username,
+        password,
+        profilePic,
+      });
+
+      return res.json({ success: true, user: newUser });
+    } catch (err) {
+      return res.status(400).json({ success: false, msg: err });
     }
   };
 
   getUserApplications = async (req, res) => {
-    const {userId} = req.params;
+    const { userId } = req.params;
 
     try {
       const userWithApplications = await this.model.findOne({
-        where: {id: userId},
-        include: [{ model: this.applicationsModel, include: this.applicationStatusModel }]
+        where: { id: userId },
+        include: [
+          {
+            model: this.applicationsModel,
+            include: this.applicationStatusModel,
+          },
+        ],
       });
 
       if (!userWithApplications) {
-        return res.status(404).json({ success: false, msg: "User not found"});
+        return res.status(404).json({ success: false, msg: "User not found" });
       }
 
-      return res.json({ success: true, applications: userWithApplications.applications })
+      return res.json({
+        success: true,
+        applications: userWithApplications.applications,
+      });
     } catch (err) {
-      return res.status(500).json({ success: false, msg: err.message })
+      return res.status(500).json({ success: false, msg: err.message });
     }
-  }
+  };
+
+  getOneUserApplication = async (req, res) => {
+    const { userId, applicationId } = req.params;
+
+    try {
+      // Find all applications data matching userId
+      const userWithApplications = await this.model.findOne({
+        where: { id: userId },
+        include: [
+          {
+            model: this.applicationsModel,
+            include: this.applicationStatusModel,
+          },
+        ],
+      });
+
+      if (!userWithApplications) {
+        return res.status(404).json({ success: false, msg: "User not found" });
+      }
+
+      const applicationIdToFind = parseInt(applicationId);
+
+      // Filter the required application id
+      const foundApplication = userWithApplications.applications.find(
+        (app) => app.id === applicationIdToFind
+      );
+
+      if (!foundApplication) {
+        return res
+          .status(404)
+          .json({ success: false, msg: "Application not found" });
+      }
+
+      return res.json({
+        success: true,
+        application: foundApplication,
+      });
+    } catch (err) {
+      return res.status(500).json({ success: false, msg: err.message });
+    }
+  };
 }
 
 module.exports = UserController;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-class UserRouter{
-  constructor(userController){
+class UserRouter {
+  constructor(userController) {
     this.userController = userController;
   }
 
-  routes=() => {
+  routes = () => {
     router.get("/", this.userController.test);
     router.get("/base", this.userController.baseMethod);
     router.get("/all", this.userController.getAll);
@@ -15,9 +15,13 @@ class UserRouter{
       "/:userId/applications",
       this.userController.getUserApplications
     ); // route for getting a user's applications
+    router.get(
+      "/:userId/:applicationId",
+      this.userController.getOneUserApplication
+    ); // route for getting a single application for a user
     router.post("/newUser", this.userController.createOne);
     return router;
-  }
+  };
 }
 
 module.exports = UserRouter;
