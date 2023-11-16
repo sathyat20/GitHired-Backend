10457b69a6c10711d5f1438cebc6c095e2b8cc4e
gbrllim <gbrllim@gmail.com>
2023-11-16 17:12:33 +0800

    Added new method (retrieveEmailFromToken) and new route

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,18 +15,24 @@ class AuthController extends BaseController {
   loginUser = async (req, res) => {
     const user = await this.model.findOne({ where: { email: req.body.email } });
 
+    // New User (Sign Up) - Redirect to /onboarding after verification
     if (!user) {
       try {
-        const token = jwt.sign({ email: req.body.email }, process.env.JWT_SECRET, {
-          expiresIn: "1h",
-        });
+        const token = jwt.sign(
+          { email: req.body.email },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
         await sendSignUpEmail({ email: req.body.email, token });
       } catch (e) {
         console.error("Login error:", e);
-        return res.status(500).send("Error logging in, please try again.");
+        return res.status(500).send("Error Signing Up, please try again.");
       }
     }
 
+    // Existing User (Sign In) - Redirect to /dashboard after verification
     if (user != null) {
       try {
         const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
@@ -42,9 +48,21 @@ class AuthController extends BaseController {
     res.send("Please check your email to finish logging in!");
   };
 
+  retrieveEmailFromToken = async (req, res) => {
+    const token = req.query.token;
+    if (token == null) return res.sendStatus(401).send("No token submitted");
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("Decoded token is:", decodedToken);
+      const email = decodedToken.email;
+      res.send(email);
+    } catch (e) {
+      return res.status(400).json({ success: false, msg: "Invalid Token" });
+    }
+  };
+
   verifyUser = async (req, res) => {
     const token = req.query.token;
-    console.log(token, "hello");
     if (token == null) return res.sendStatus(401);
     try {
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
