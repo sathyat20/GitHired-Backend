974949f7975870b833c25b1ed0705bb398f59536
gbrllim <gbrllim@gmail.com>
2023-11-17 01:13:25 +0800

    DARK MODE MAILER

--- mailer.js
+++ mailer.js
@@ -7,20 +7,26 @@ function sendSignUpEmail({ email, token }) {
       to: email,
       from: process.env.FROM_EMAIL,
       subject: "GitHired: Onboarding Time! 🧑🏻‍💻",
-      html: `<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
-          <h1 style="color: #333; text-align: center;">Welcome to GitHired!</h1>
-          <p style="font-size: 16px; color: #555; text-align: center;">
-            Thanks for signing up. Click below to finish the registration process.
-          </p>
-          <div style="text-align: center; margin-top: 20px;">
-            <a 
-              href="${process.env.FRONTEND_URL}/onboarding?token=${token}"
-              style="display: inline-block; padding: 12px 20px; background-color: #4285f4; color: #fff; text-decoration: none; border-radius: 5px; font-weight: bold;"
-            >
-              Onboarding
-            </a>
-          </div>
-        </div>`,
+      html: `
+      <div style="font-family: Arial, sans-serif; padding: 20px; border-radius: 25px; max-width: 800px; margin: 0 auto; background-color: #050b1e; color: white;">
+        <!-- Logo -->
+        <div style="text-align: center; margin-bottom: 20px;">
+          <img src="https://firebasestorage.googleapis.com/v0/b/githired-c0060.appspot.com/o/profile-images%2FGitHired.png?alt=media&token=38f945e7-0900-49c4-9cd7-d44b056edb4f" alt="GitHired Logo" style="max-width: 600px; height: auto;">
+        </div>
+        <!-- Heading and content -->
+        <h1 style="color: white; text-align: center;">Welcome to GitHired!</h1>    
+        <p style="font-size: 16px; color: white; text-align: center;">
+          Thanks for signing up. Click below to finish the registration process.
+        </p>
+        <div style="text-align: center; margin-top: 20px;">
+          <a 
+            href="${process.env.FRONTEND_URL}/onboarding?token=${token}"
+            style="display: inline-block; padding: 12px 20px; background-color: #1C3F58; color: white; text-decoration: none; border-radius: 5px; font-weight: bold;"
+          >
+            Onboard
+          </a>
+        </div>
+      </div>`,
     })
     .then(() => {
       console.log("Email sent");
@@ -37,20 +43,26 @@ function sendSignInEmail({ email, token }) {
       to: email,
       from: process.env.FROM_EMAIL,
       subject: "GitHired: Your Magic Link is here! 🪄",
-      html: `<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
-          <h1 style="color: #333; text-align: center;">Welcome back to GitHired!</h1>
-          <p style="font-size: 16px; color: #555; text-align: center;">
-            Click the magic link below to finish log in.
-          </p>
-          <div style="text-align: center; margin-top: 20px;">
-            <a 
-              href="${process.env.FRONTEND_URL}/dashboard?token=${token}"
-              style="display: inline-block; padding: 12px 20px; background-color: #4285f4; color: #fff; text-decoration: none; border-radius: 5px; font-weight: bold;"
-            >
-              Log In
-            </a>
-          </div>
-        </div>`,
+      html: ` 
+      <div style="font-family: Arial, sans-serif; padding: 20px; border-radius: 25px; max-width: 800px; margin: 0 auto; background-color: #050b1e; color: white;">
+        <!-- Logo -->
+        <div style="text-align: center; margin-bottom: 20px;">
+          <img src="https://firebasestorage.googleapis.com/v0/b/githired-c0060.appspot.com/o/profile-images%2FGitHired.png?alt=media&token=38f945e7-0900-49c4-9cd7-d44b056edb4f" alt="GitHired Logo" style="max-width: 600px; height: auto;">
+        </div>
+        <!-- Heading and content -->
+        <h1 style="color: white; text-align: center;">Welcome back to GitHired!</h1>    
+        <p style="font-size: 16px; color: white; text-align: center;">
+          Click the magic link below to finish log in.
+        </p>
+        <div style="text-align: center; margin-top: 20px;">
+          <a 
+            href="${process.env.FRONTEND_URL}/dashboard?token=${token}"
+            style="display: inline-block; padding: 12px 20px; background-color: #1C3F58; color: white; text-decoration: none; border-radius: 5px; font-weight: bold;"
+          >
+            Log In
+          </a>
+        </div>
+      </div>`,
     })
     .then(() => {
       console.log("Email sent");
